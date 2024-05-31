b94e11eaa6b300d0ed7cf450a43d54a12d5cbcf8
Becky Sweger <rsweger@umass.edu>
2024-05-31 09:37:36 -0400

    Add Flusight database and some dependencies to access it

--- src/flusight/__init__.py
+++ src/flusight/__init__.py
@@ -1,1 +1,8 @@
+from importlib import resources
+
+from cloudpathlib import AnyPath
+
 __version__ = "0.0.1"
+
+MODULE_PATH = AnyPath(resources.files("flusight")).parent
+DATA_PATH = MODULE_PATH / "data"
